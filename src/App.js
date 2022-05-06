06d95433e978e2d8a436e6f9e0624e544fb52d8c
Rafael Solano <nemesysolano@gmail.com>
2022-05-06 09:43:30 -0400

    Added React's hot reloader

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { hot } from 'react-hot-loader';
+
 import './App.css';
 
 const App = () => {
-    return (<div className="app"><h1>Hey</h1></div>)
+    return (<div className="app"><h1>Laid Foundations</h1></div>)
 }
 
-export default App;
+export default hot(module)(App);
